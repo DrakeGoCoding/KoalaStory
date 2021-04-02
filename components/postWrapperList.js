4b890475a3178ea3366cea2f2904366b37946eb0
DrakeGoCoding <hoanglongphi21@gmail.com>
2021-04-02 20:23:29 +0700

    Update postWrapperList.js

--- components/postWrapperList.js
+++ components/postWrapperList.js
@@ -3,7 +3,7 @@ import { getDataFromDoc, getItemFromLocalStorage, compareCreatedDate, getPublicP
 export class PostWrapperList extends HTMLElement {
     constructor() {
         super();
-        this.shadowDom = this.attachShadow({ mode: 'open' });      
+        this.shadowDom = this.attachShadow({ mode: 'open' });
     }
 
     async connectedCallback() {
@@ -36,10 +36,10 @@ export class PostWrapperList extends HTMLElement {
             .where('isPublic', '==', true)
             .onSnapshot(snapShot => {
                 if (firstRun) firstRun = false;
-                else{
+                else {
                     const docChanges = snapShot.docChanges();
-                    for (const docChange of docChanges){
-                        if (docChange.type === 'added'){
+                    for (const docChange of docChanges) {
+                        if (docChange.type === 'added') {
                             this.appendNewPost(getDataFromDoc(docChange.doc));
                         }
                     }
@@ -47,13 +47,13 @@ export class PostWrapperList extends HTMLElement {
             });
     }
 
-    appendNewPost(data){
+    appendNewPost(data) {
         const currentUser = getItemFromLocalStorage('currentUser');
         const newPostWrapper = document.createElement("post-wrapper");
         newPostWrapper.setAttribute('user-name', currentUser.fullName);
         newPostWrapper.setAttribute('created-time', data.createdDate);
         newPostWrapper.setAttribute('content', data.content);
-        
+
         const postList = this.shadowDom.querySelector('.post-list');
         postList.insertBefore(newPostWrapper, postList.firstChild);
     }
